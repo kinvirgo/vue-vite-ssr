f28166a93ae0c5d9064fbd1b32ef8da94305751d
kinvirgo <jiangjinyu@ijovo.com>
2022-02-14 10:54:32 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
-<template>
-    <router-link to="/">首页→</router-link> | <router-link to="/client">客户端→</router-link> |
-    <router-link to="/server">服务端→</router-link>
-    <router-view></router-view>
-</template>
-<script lang="ts">
-import { defineComponent } from "vue";
-export default defineComponent({
-    setup(props, context) {
-        return {};
-    },
-});
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
-<style lang="scss"></style>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
