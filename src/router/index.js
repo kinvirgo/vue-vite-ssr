3003a391660a5ea792062046da0f3d786b9e6d92
kinvirgo <jiangjinyu@ijovo.com>
2022-02-17 15:54:50 +0800

    update:1.删除无用代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,28 @@
-import {
-    createMemoryHistory,
-    createRouter as _createRouter,
-    createWebHistory,
-} from 'vue-router'
+import { createMemoryHistory, createRouter as _createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
     {
         path: '/',
         component: () => import('@/views/home.vue'),
-        meta: { __id: 'home' },
+        meta: {},
         children: [
             {
                 path: 'footer',
                 component: () => import('@/views/footer.vue'),
-                meta: { __id: 'footer' },
+                meta: {},
             },
         ],
     },
     {
         path: '/about',
         component: () => import('@/views/about.vue'),
-        meta: { __id: 'footer' },
+        meta: {},
     },
 ]
 
 export function createRouter(base) {
     return _createRouter({
-        history: import.meta.env.SSR
-            ? createMemoryHistory(base)
-            : createWebHistory(base),
+        history: import.meta.env.SSR ? createMemoryHistory(base) : createWebHistory(base),
         routes,
     })
 }
