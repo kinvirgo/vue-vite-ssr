31898a2faeae89d30749185307e0d5710fdd771a
kinvirgo <jiangjinyu@ijovo.com>
2022-03-04 10:18:37 +0800

    fix:修复本地开发路由访问问题

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/ssr/',
+    base: process.env.NODE_ENV === 'production' ? '/ssr/' : '/',
     plugins: [vue()],
     resolve: {
         alias: {
